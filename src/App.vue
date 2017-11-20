455651918c61fe7624cf7bc880a6aca0e80b8a4e
Alammm ubuntuCoopeuch <alan.munozlopez@gmail.com>
2017-11-20 17:46:46 -0300

    proyecto base

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  #app
+    h1 {{ msg }}
+    p {{ 1+1 }}
+    p {{ 'Hola' + 'Mundo' }}
+    p {{ person.name }}
+    p {{ person.name.toUpperCase() }}
+    p {{ true? 'true' : 'false' }}
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App!',
+
+      person: {
+        name: 'Alan'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss'
+</style>
